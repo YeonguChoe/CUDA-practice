f15fce195a1058cc2272d460af55a575fb35facf
YeonguChoe <yeongu.choe@icloud.com>
2025-11-14 18:11:38 -0500

    Update main.cu

--- main.cu
+++ main.cu
@@ -31,3 +31,58 @@ int main()
 
     return 0;
 }
+
+
+//
+
+#include <iostream>
+using namespace std;
+
+__global__ void addition(int *a, int *b)
+{
+    printf("Block index: %d\n", blockIdx.x);
+    printf("Block dimension: %d\n", blockDim.x);
+    printf("Thread index: %d\n", threadIdx.x);
+    *(a + threadIdx.x) = *(a + threadIdx.x) * *(b + threadIdx.x);
+
+
+    int id = blockIdx.x* blockDim.x + threadIdx.x;
+
+}
+
+int main()
+{
+    int a[] = {1, 2, 3, 4, 5};
+    int *ptr_a = a;
+
+    int b[] = {10, 20, 30, 40, 50};
+    int *ptr_b = b;
+
+    int *device_a;
+    if (cudaMalloc(&device_a, sizeof(int) * 5) == cudaSuccess)
+    {
+        cout << "Successful" << endl;
+    }
+
+    int *device_b;
+    cudaMalloc(&device_b, sizeof(b));
+
+    if (cudaMemcpy(device_a, ptr_a, sizeof(a), cudaMemcpyHostToDevice) == cudaSuccess)
+    {
+        cout << "cuda memcopy Successful" << endl;
+    }
+
+    cudaMemcpy(device_b, ptr_b, sizeof(int) * 5, cudaMemcpyHostToDevice);
+
+    addition<<<1, 5>>>(device_a, device_b);
+
+    cudaMemcpy(ptr_a, device_a, sizeof(a), cudaMemcpyDeviceToHost);
+
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     std::cout << a[i] << std::endl;
+    // }
+
+    cudaFree(device_a);
+    cudaFree(device_b);
+}
